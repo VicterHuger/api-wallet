eb342d0476478503705b7ad854e0a9ba5bc7e6f2
Victor Hugo <victorhugobs@eq.ufrj.br>
2022-07-01 23:24:50 -0300

    fix: corrects the message of error sent when an user misses some sign in critereas

--- src/middlewares/userSchemasValidationMiddleware.js
+++ src/middlewares/userSchemasValidationMiddleware.js
@@ -4,19 +4,21 @@ import userSchemaSignUp from '../schemas/userSchemaSignUp.js';
 async function userSchemaSignUpValidationMiddleware(req,res,next){
     const user=req.body;
     const userSchema=userSchemaSignUp;
+    const authRequest='sign-up';
     
     res.locals.userSchema=userSchema;
     res.locals.user=user;
-    
+    res.locals.authRequest=authRequest;
     next();
 }
+
 async function userSchemaSignInValidationMiddleware(req,res,next){
     const user=req.body;
     const userSchema=userSchemaSignIn;
-    
+    const authResquest='sign-in';
     res.locals.userSchema=userSchema;
     res.locals.user=user;
-    
+    res.locals.authRequest=authResquest;
     next();
 }
 
--- src/middlewares/verifyUserSchemasErrorMiddleware.js
+++ src/middlewares/verifyUserSchemasErrorMiddleware.js
@@ -1,10 +1,19 @@
 async function verifyUserSchemasErrorMiddleware(req,res,next){
     const error=res.locals.error;
     const user=res.locals.user;
-   
+    const authRequest=res.locals.authRequest;
     if(error){
         const errorMessages=error.details.map(item=>item.message);
-        return res.status(404).send(errorMessages);
+        let message='';
+        if(/password/.test(errorMessages) && authRequest==='sign-up' ){
+            message='Senha fraca/moderada -> Digite uma senha de pelo menos 8 dígitos, 1 caractere especial, 1 número, 1 letra minúscula e 1 letra maiúscula';    
+        }else if(/password/.test(errorMessages) && authRequest==='sign-in' ){
+            message='Dados inválidos! Verifique os dados inseridos!';  
+        }else{
+            message=errorMessages.join(';');
+        }
+        
+        return res.status(422).send(message);
     }
     res.locals.user=user;
     next();
